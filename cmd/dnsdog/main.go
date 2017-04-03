bc488d7dbd8090d35facd1c9655d2594c419b0cf
Michael Barrett <loki77@gmail.com>
2017-04-03 14:41:04 -0700

    Only tag stats with query when enabled
    This gives dnsdog a CLI flag that enables whether or not it should tag
    its stats with the query (ie: the hostname of the DNS packet), and
    disables this feature by default. This should cut down on the
    cardinality of our dns stats, which is quite high right now.

--- cmd/dnsdog/main.go
+++ cmd/dnsdog/main.go
@@ -9,11 +9,12 @@ import (
 
 func main() {
 	var (
-		iface = flag.String("iface", "eth0", "Interface to listen on")
-		addr  = flag.String("statsd", "127.0.0.1:8125", "Statsd address")
+		iface        = flag.String("iface", "eth0", "Interface to listen on")
+		addr         = flag.String("statsd", "127.0.0.1:8125", "Statsd address")
+		includeQuery = flag.Bool("query", false, "Whether to include query strings in stats, defaults to false.")
 	)
 	flag.Parse()
-	if err := dnsdog.Watch(*iface, *addr); err != nil {
+	if err := dnsdog.Watch(*iface, *addr, *includeQuery); err != nil {
 		log.Fatal(err)
 	}
 }
